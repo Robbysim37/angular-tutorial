a61de068249d6b2fc26c9709ad16a024701fdf0e
Robert Lewis <Robbysim37@gmail.com>
2023-12-26 15:14:22 -0500

    angular practice

--- first-angular-app/src/app/server/server.component.ts
+++ first-angular-app/src/app/server/server.component.ts
@@ -3,8 +3,22 @@ import { Component } from "@angular/core"
 @Component({
     //HTML tag to access component
     selector: `app-server`,
+    // Pointer to the HTML file we want to use as a template
     templateUrl: "./server.component.html"
 })
 export class ServerComponent {
+    serverId: number = 10;
+    serverStatus: string = "offline";
 
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? "online" : "offline"
+    }
+
+    getServerStatus() {
+        return this.serverStatus
+    }
+
+    getColor() {
+        return this.serverStatus === "online" ? "green" : "red";
+    }
 }
--- first-angular-app/src/app/servers/servers.component.ts
+++ first-angular-app/src/app/servers/servers.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
+  serverName = "Testserver";
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!'
+  serverCreated = false;
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+  }
+
+  ngOnItit(){
+
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = `Server was created! Name is ${this.serverName}`
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
